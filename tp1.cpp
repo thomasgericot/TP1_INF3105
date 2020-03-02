2a581702f6c4811a4cb1d0e54c65b456a84bc099
thomasgericot <tomtomgericot@gmail.com>
2020-03-01 21:28:38 -0500

    le logiciel fonctionne

--- tp1.cpp
+++ tp1.cpp
@@ -12,15 +12,26 @@
 #include <limits>
 #include "tableau.h"
 #include "quartier.h"
+#include"math.h"
 
 
 using namespace std;
+float calcul_pourcentage(int nombre_menage_total, int nombre_sous_seuil){
+    return nombre_sous_seuil/nombre_menage_total;
+}
+
+int seuil_pauvrete(int nbPersonnes){
+    return (7000+850*(nbPersonnes-2));
+}
 
 int tp1(istream& entree){
     
     Tableau<Quartier> re1= Tableau<Quartier>(8);
+    Tableau<Quartier> re2= Tableau<Quartier>(8);
     std::string line;
     bool ajouterFoyer=false;
+    Quartier quartier_bas;
+    Quartier quartier_haut;
     
     
     Quartier quartier;
@@ -49,20 +60,106 @@ int tp1(istream& entree){
         }
     }
     
+    //Affichage sur la sortie standart:
+    
+    
     // calcul revenu moyen par quartier
+    cout<<"a) ";
     int moyenne = 0;
     for(int i=0;i<re1.nbElements;i++)
     {
         moyenne += re1[i].MoyenneQuartier();
     }
     moyenne /=re1.nbElements;
+    cout<<"Revenu moyen:\t"<<round(moyenne)<<endl;
     
-    cout<<moyenne<<endl;
+    
+        
+    for(int i=0;i<re1.nbElements;i++)
+    {
+        if(re1[i].MoyenneQuartier()<moyenne)
+        {
+                re2.ajouter(re1[i]);
+        }
+        
+        
+            }
     
     
-return 0;
+    cout<<"b) Liste des quartiers defavorables.\n";
+    for(int i=0;i<re2.nbElements;i++)
+    {
+        
+        cout<<re2[i].NomQuartier<<"\t"<<"Revenu moyen: "<<re2[i].MoyenneQuartier()<<endl;
+    }
+    
+    
+    int revenu_quartier_bas=re1[0].MoyenneQuartier();
+    int revenu_quartier_haut=re1[0].MoyenneQuartier();
+    quartier_bas.NomQuartier=re1[0].NomQuartier;
+    quartier_haut.NomQuartier=re1[0].NomQuartier;
+    
+    
+    for(int i=0;i<re1.nbElements;i++){
+        
+    if(re1[i].MoyenneQuartier()<revenu_quartier_bas)
+    {
+        revenu_quartier_bas=re1[i].MoyenneQuartier();
+        quartier_bas.NomQuartier=re1[i].NomQuartier;
+    }
+        
+    if(re1[i].MoyenneQuartier()>revenu_quartier_haut)
+    {
+        revenu_quartier_haut=re1[i].MoyenneQuartier();
+        quartier_haut.NomQuartier=re1[i].NomQuartier;
+    }
+        
+    }
+    
+    cout<<"c)\n";
+    cout<<quartier_bas.NomQuartier<<"\t\t"<<"Quartier avec le plus petit revenu moyen: "<<revenu_quartier_bas<<endl;
+    cout<<quartier_haut.NomQuartier<<"\t\t"<<"Quartier avec le plus grand revenu moyen: "<<revenu_quartier_haut<<endl;
+    
+    
+    
+    
+    int nombre_sous_seuil=0;
+    int nombre_menage_total=0;
+    
+    
+    for(int i=0;i<re1.nbElements;i++){
+        
+        for(int j=0;j<re1[i].foyers.nbElements;j++){
+            
+            nombre_menage_total=nombre_menage_total+re1[i].foyers[j].NombreMenage;
+            
+            if (re1[i].foyers[j].revenu < seuil_pauvrete(re1[i].foyers[j].NombreMenage))
+            {
+                
+                nombre_sous_seuil=nombre_sous_seuil+1;
+                
+            }
+          }
+        }
+    
+    
+    cout<<nombre_sous_seuil<<endl;
+    cout<<nombre_menage_total<<endl;
+    
+    
+    cout<<"d) Pourcentage de menages dont le revenu est faibles: "<<((double)nombre_sous_seuil/(double)nombre_menage_total)*100<<endl;
+    
+    return 0;
 }
-// syntaxe d'appel : ./tp1 [nomfichier.txt]
+
+
+
+
+
+
+
+
+
 int main(int argc, const char** argv){
     // Gestion de l'entrée :
     //  - lecture depuis un fichier si un argument est spécifié;
